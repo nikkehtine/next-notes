5ae243fbdafe18e8b5653663f2dc24ccf5f57b5b
nikkehtine <27138416+nikkehtine@users.noreply.github.com>
2025-03-25 16:05:18 +0100

    Fix deploy

--- src/components/theme-provider.tsx
+++ src/components/theme-provider.tsx
@@ -1,12 +1,12 @@
 "use client";
 
 import { useTheme } from "@/hooks/use-theme";
-import { ReactNode, useEffect, useMemo } from "react";
+import { ReactNode, useEffect } from "react";
 
 // TODO: Fix FOUC
 
 export const ThemeProvider = ({ children }: { children: ReactNode }) => {
-  const [theme, setTheme] = useTheme();
+  const [theme] = useTheme();
 
   useEffect(() => {
     document.body.classList.toggle("dark", theme === "dark");
